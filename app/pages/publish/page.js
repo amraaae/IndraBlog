4312e738d65fa73ea28d1fa30d953ccebdf2fd1e
amraaae <AMRAAAE6@GMAIL.COM>
2024-04-12 16:29:45 +0800

    a

--- app/pages/publish/page.js
+++ app/pages/publish/page.js
@@ -21,21 +21,30 @@ function Posts() {
     }
   };
   return (
-    <div className="h-[93.5vh] w-full">
+    <div className="h-[93.5vh] w-full flex flex-col justify-center items-center">
       <input
         type="text"
+        className="w-[300px] px-4 py-2 text-black mb-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+        placeholder="Enter title"
         onChange={(e) => {
           setTitle(e.target.value);
         }}
       />
       <textarea
+        className="w-[300px] px-4 py-2 text-black mb-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 resize-none "
+        placeholder="Enter description"
         onChange={(e) => {
           setDescription(e.target.value);
         }}
       />
       <FileUpload setName={setFileName} />
-      <button onClick={handlePublish}>publish</button>
-      {success && <p>success</p>}
+      <button
+        className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+        onClick={handlePublish}
+      >
+        Publish
+      </button>
+      {success && <p className="mt-4 text-green-600">Success</p>}
     </div>
   );
 }
--- components/fileUpload.jsx
+++ components/fileUpload.jsx
@@ -46,13 +46,12 @@ function FileUpload({ setName }) {
     selectFile && upload();
   }, [selectFile]);
   return (
-    <div>
-      {/* {success & <p>{success}</p>} */}
+    <div className="mt-4 mb-4">
       <input
+        id="file_input"
         type="file"
-        onChange={(e) => {
-          fileSelectHandle(e);
-        }}
+        className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+        onChange={(e) => fileSelectHandle(e)}
       />
     </div>
   );
